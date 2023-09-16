2b903a16c4c72ae42b404f0a492cbcd77a6ab70c
adan.gros <adan.gros@gmail.com>
2023-09-15 22:21:58 -0300

    gdfg

--- acciones/equipos/nuevo.php
+++ acciones/equipos/nuevo.php
@@ -1,6 +1,8 @@
 <?php
-
-
+require_once '../../modelo/Jugador.php';
+require_once 'responses/nuevoResponse.php';
+require_once 'request/nuevoRequest.php';
+header('Content-Type: application/json');
 
 $Resp=new NuevoResponse;
 
@@ -8,14 +10,18 @@ $json=file_get_contents('php://input',true);
 $req=json_decode($json);
 $CantidadJugadores=0;
 
+foreach($req->ListJugadores as $j){
+$CantidadJugadores=$CantidadJugadores + 1;
+}
+
 if($CantidadJugadores>=1 && $CantidadJugadores<=5){
 $Resp->ItsOk=true;
 $Resp->Mensaje=' ';
 }else {
 $Resp->IsOk= false;
-$Resp->Mensaje="El equipo posee [cantidadDeJugadoresIndicados] y debe poseer entre 1 y 5  jugadores"
+$Resp->Mensaje="El equipo posee " . $CantidadJugadores." y debe poseer entre 1 y 5  jugadores";
   
-};
+}
 
 echo json_encode($Resp);
 
